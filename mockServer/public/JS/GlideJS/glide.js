2ce2f4332a6414e0aec380246ae5688e89f76654
mauriciogaray10 <garaycarlosmauricio17@gmail.com>
2023-12-04 16:59:13 -0300

    server added

---
+++ mockServer/public/JS/GlideJS/glide.js
@@ -0,0 +1,14 @@
+new Glide(".glide", {
+  type: "carousel",
+  startAt: 0,
+  perView: 3,
+  gap: 30,
+  breakpoint: {
+    991: {
+      perview: 2,
+    },
+    768: {
+      perview: 1,
+    },
+  },
+}).mount();
