ad01f52c4f49ca51ec5795a706cf1bb1a879cab1
mauriciogaray10 <garaycarlosmauricio17@gmail.com>
2023-12-11 10:55:19 -0300

    db connected

---
+++ src/models/config/conn.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: process.env.DB,
+  port: process.env.DBPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = {
+  conn: pool.promise(),
+};
